1262343046b82fa3cd3b559cb8ae3015d4c84a01
Anaru03 <dele22428@uvg.edu.gt>
2025-02-24 22:14:17 -0600

    Front Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import ScrollButton from '@components/ScrollButton'; 
 import Footer from '@components/Footer'; 
-import './App.css'
+import { Hourglass } from 'react-loader-spinner'; 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <> 
-      <div className="bg-stone-950 font-serif">
+      {
+        loading ?
+        <div className='bg-stone-950 h-screen flex justify-center items-center'>
+          <Hourglass
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="hourglass-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            colors={['rgb(199 52 58)', 'rgb(255 225 217)']}
+          />
+        </div>
+        :
+        <div className="bg-stone-950 font-serif">
+          <ScrollButton />
           <Footer />
         </div>
+      }
     </>
-  )
+  );
 }
 
 export default App
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 const Footer = () => {
     return (
-        <footer>
+        <footer className="bg-stone-900 text-violet-400 lg:px-10 px-4 py-3">
             <div className="container mx-auto grid grid-cols-1 lg:flex lg:justify-between">
                 <div className="mb-4 md:mb-0 flex items-center">
-                    <img src="src/assets/Icono.png" alt="Logo" className="w-8 h-8 lg:w-6 lg:h-6 mr-4"/>
-                    <div>
-                    </div>
+                    <img src="src/assets/Icono.png" alt="Logo" className="w-1/6 h-auto lg:w-1/12 mr-4"/>
                 </div>
             </div>
+
             <div className="text-white text-center lg:text-center">
                 <span>© 2024 Equipo Alfa Buena Maravilla. All rights reserved.</span>
             </div>
